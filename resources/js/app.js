2d8214a42e6655d57ee9851c2ae9ac8efa662e06
Carlos Avila Aguilar <krloz1003@gmail.com>
2021-10-07 12:40:30 -0500

    Starting vuetify implementation

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,10 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css'
+Vue.use(Vuetify);
 
 /**
  * The following block of code may be used to automatically register your
@@ -30,4 +34,5 @@ Vue.component('cmp-product', require('./components/products/CmpProduct.vue').def
 
 const app = new Vue({
     el: '#app',
+    vuetify: new Vuetify()
 });
