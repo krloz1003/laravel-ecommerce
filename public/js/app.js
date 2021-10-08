0a35a4ae5c5f1564ab7df2d55d7057f0f2ccd1f0
Carlos Avila Aguilar <krloz1003@gmail.com>
2021-10-07 20:18:41 -0500

    Resolving conflict in scripts

--- public/js/app.js
+++ public/js/app.js
@@ -50120,8 +50120,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\laragon\www\laravel-ecommerce\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\laragon\www\laravel-ecommerce\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\cavila\Documents\laragon\www\laravel-ecommerce\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\cavila\Documents\laragon\www\laravel-ecommerce\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
