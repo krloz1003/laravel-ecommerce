0464ffccb46d25dfead65db994ace6ee169f609f
Carlos Avila Aguilar <krloz1003@gmail.com>
2021-10-07 20:11:44 -0500

    Applying styling to templates the project

--- resources/js/components/products/CmpProduct.vue
+++ resources/js/components/products/CmpProduct.vue
@@ -1,81 +1,73 @@
-<template>    
-  <div class="text-center">
-      <h1>Iniciando el componenete de productos</h1>
-    <v-btn
-      class="mx-2"
-      fab
-      dark
-      small
-      color="primary"
-    >
-      <v-icon dark>
-        mdi-minus
-      </v-icon>
-    </v-btn>
+<template>
+<v-row dense>
+	<v-col
+		v-for="row in rows"
+		:key="row.id"
+		:cols="3"
+	>    
+		<v-card 
+			class="mx-auto my-12"
+			max-width="374"
+		>
+			<template slot="progress">
+				<v-progress-linear
+					color="deep-purple"
+					height="10"
+					indeterminate
+				></v-progress-linear>
+			</template>
+			<v-img
+				height="150"
+				src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+			></v-img>
+			<v-card-title>{{ row.name }}</v-card-title>
+			<v-card-text>
+				<v-row
+					align="center"
+					class="mx-0"
+				>
+				</v-row>
+				<div class="my-4 text-subtitle-1">
+					$ {{ row.price }}
+				</div>
+				<div>{{ row.description }}</div>
+			</v-card-text>
+			<v-divider class="mx-4"></v-divider>
 
-    <v-btn
-      class="mx-2"
-      fab
-      dark
-      small
-      color="pink"
-    >
-      <v-icon dark>
-        mdi-heart
-      </v-icon>
-    </v-btn>
-
-    <v-btn
-      class="mx-2"
-      fab
-      dark
-      color="indigo"
-    >
-      <v-icon dark>
-        mdi-plus
-      </v-icon>
-    </v-btn>
-
-    <v-btn
-      class="mx-2"
-      fab
-      dark
-      color="teal"
-    >
-      <v-icon dark>
-        mdi-format-list-bulleted-square
-      </v-icon>
-    </v-btn>
-
-    <v-btn
-      class="mx-2"
-      fab
-      dark
-      large
-      color="cyan"
-    >
-      <v-icon dark>
-        mdi-pencil
-      </v-icon>
-    </v-btn>
-
-    <v-btn
-      class="mx-2"
-      fab
-      dark
-      large
-      color="purple"
-    >
-      <v-icon dark>
-        mdi-android
-      </v-icon>
-    </v-btn>
-  </div>
+			<v-card-actions>
+				<v-btn
+					color="success"
+					@click="reserve"
+				>
+					ADD TO CAR
+				</v-btn>
+			</v-card-actions>
+		</v-card>
+	</v-col>
+</v-row>
 </template>
 <script>
-    export default {
-        mounted() {
-            console.log('Component mounted.')
-        }
-    }
+	export default {
+		mounted() {
+			console.log('Cargando la lista de productos');
+			this.getProducts();
+		},
+		data: () => ({
+			loading: false,
+			selection: 1,
+			rows: []
+		}),
+		methods: {
+			reserve () {
+				this.loading = true
+				setTimeout(() => (this.loading = false), 2000)
+			},
+			getProducts(){
+				axios.get( `${Laravel.baseUrl}/api/products`)
+				.then(response => {
+					this.rows = response.data;
+				});
+			},
+		}
+	}
 </script>
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Pruebas en desarrollo</h1>
-<cmp-product></cmp-product> 
+
+	<cmp-product></cmp-product> 
 @endsection
