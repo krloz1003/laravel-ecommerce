7a6bfb707c12b7266ffed632816e5e59f5be816f
Carlos Avila Aguilar <krloz1003@gmail.com>
2021-10-10 11:19:24 -0500

    Establishing api routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/products', 'ProductController@getData');
+Route::get('/product/{slug}', 'ProductController@show');
+Route::put('/product/{id}', 'ProductController@update');
+Route::post('/product', 'ProductController@store');
+Route::delete('/product/{id}', 'ProductController@destroy');
+
+Route::get('/users', 'UserController@getData');
